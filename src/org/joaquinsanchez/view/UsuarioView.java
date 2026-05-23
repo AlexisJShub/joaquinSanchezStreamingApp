efd1f8a3f8bd78e547cfb91de8ad1980b1f4c1ea
joaquinsanchez <jsanchez-2026117@kinal.edu.gt>
2026-05-22 19:29:21 -0600

    001 Se han agregado los packages y clases correspondientes

---
+++ src/org/joaquinsanchez/view/UsuarioView.java
@@ -0,0 +1,14 @@
+package org.joaquinsanchez.view;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author joaqu
+ */
+public class UsuarioView {
+    
+}
